745177af27ba37373b4d8358f938130d1f1b55bd
the_freestyle <the_freestyle@abv.bg>
2019-03-10 01:51:14 +0200

    Update

---
+++ 01. Intro and basic syntax - Exercise/06. Strong number/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace _06._Strong_number
+{
+    using System;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int saveNumber = number;
+
+            int result = 0;
+
+            while (number != 0)
+            {
+                int factoriel = 1;
+
+                int currentNumber = number % 10;
+
+                number = number / 10;
+
+                for (int i = 1; i < currentNumber; i++)
+                {
+                    factoriel += i * factoriel;
+                }
+
+                result += factoriel;
+            }
+
+            if (result == saveNumber)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
