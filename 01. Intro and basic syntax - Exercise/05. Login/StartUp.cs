76b8cb61f9a83b758f3901e29451797a64de4fcb
the_freestyle <the_freestyle@abv.bg>
2019-03-10 01:44:01 +0200

    Update

---
+++ 01. Intro and basic syntax - Exercise/05. Login/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace _05._Login
+{
+    using System;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string password = string.Empty;
+            string username = Console.ReadLine();
+
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                password += username[i];
+
+            }
+
+            int counter = 0;
+
+            while (true)
+            {
+                string login = Console.ReadLine();
+
+                if (login == password)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    counter += 1;
+                    return;
+
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect password. Try again.");
+                    counter += 1;
+                }
+
+                if (counter == 3)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    return;
+                }
+            }
+        }
+    }
+}
